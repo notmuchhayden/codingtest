49151aed673d24f0cc1fc11d2d35a6fa53367253
seaeast2 <seaeast2@gmail.com>
2025-07-10 20:07:50 +0900

    Update 0009_Palindrome_Number.cpp
    9. Palindrome Number

--- leetcode/0009_Palindrome_Number.cpp
+++ leetcode/0009_Palindrome_Number.cpp
@@ -20,21 +20,25 @@ Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 
 Constraints:
 
--231 <= x <= 231 - 1
+-2^31 <= x <= 2^31 - 1
  
 
 Follow up: Could you solve it without converting the integer to a string?
 
 */
 
+#include <iostream>
+#include <string>
+
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // 풀이 전략
-        // 1. x 를 문자열로 변환하여 뒤집은 것과 비교하면 가능 하다.
-        // 2. 문자열로 변환하지 않고 해결하는 방법
-        //    => 숫자를 하나씩 분해하여 배열에 역으로 넣고, 자리수 만큼 
-        //        곱셈을 하여 숫자를 재구축 한다. 그리고 그 결과와 처음값 x 를 비교한다.
-    
+        std::string org = std::to_string(x);
+        std::string rvs = org;
+        std::reverse(rvs.begin(), rvs.end());
+        if (org == rvs)
+            return true;
+        else
+            return false;
     }
 };
