a08c7c3e0c788aa82bdb24829edc40ebfd3f0c53
notmuchhayden <119303558+notmuchhayden@users.noreply.github.com>
2025-07-21 23:51:59 +0900

    21번 완료
    완료

--- leetcode/0021_Merge_Two_Sorted_Lists.cpp
+++ leetcode/0021_Merge_Two_Sorted_Lists.cpp
@@ -25,31 +25,53 @@ Return the head of the merged linked list.
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+		if (list1 == nullptr && list2 == nullptr)
+			return nullptr;
 		if (list1 && list2 == nullptr)
 			return list1;
 		if (list1 == nullptr && list2)
 			return list2;
 		
-		ListNode *head = *newlist = nullptr;
+		ListNode *newHead, *newList;
+		newHead = newList = nullptr;
 		
-		while (list1 || list2) {
-			
-			
-			
-			if (list1->val < list2->val) {
-				newlist = list1;
-				list1 = list1->next;
+		ListNode* curList1 = list1;
+		ListNode* curList2 = list2;
+		
+		if (curList1->val < curList2->val) {
+			newHead = newList = curList1;
+			curList1 = curList1->next;
+		}
+		else {
+			newHead = newList = curList2;
+			curList2 = curList2->next;
+		}
+		
+		while (curList1 || curList2) {
+			if (curList1 && curList2 == nullptr) {
+				newList->next = curList1;
+				newList = newList->next;
+				curList1 = curList1->next;
+			}
+			else if (curList1 == nullptr && curList2) {
+				newList->next = curList2;
+				newList = newList->next;
+				curList2 = curList2->next;
 			}
 			else {
-				newlist = list2;
-				list2 = list2->next;
+				if (curList1->val < curList2->val) {
+					newList->next = curList1;
+					newList = newList->next;
+					curList1 = curList1->next;
+				}
+				else {
+					newList->next = curList2;
+					newList = newList->next;
+					curList2 = curList2->next;
+				}
 			}
 		}
+		
+		return newHead;
     }
-	
-	ListNode* getSmallerOne(ListNode* l1, ListNode* l2) {
-		if (list1 && list2 == nullptr) {
-			
-		}
-	}
 };
